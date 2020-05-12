76a1441a8aabe2f032537abbdc4c846a23833446
Kazuhide Uchida <uchidakaz@gmail.com>
2020-05-12 19:26:10 +0900

    page160

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,78 +1,58 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Rect from './Rect.js'
-import { queryAllByAttribute } from '@testing-library/react';
+import Rect from './Rect';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
-  msgStyle1 = {
-    fontSize:"24pt",
+
+  data = [];
+
+  msgStyle = {
+    fontSize:"20pt",
     color:"#900",
     margin:"20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
+    padding:"5px",
   }
-  msgStyle2 = {
-    fontSize:"20pt",
-    color:"white",
-    backgroundColor:"#900",
-    margin:"20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
-  }
-  btnStyle = {
-    fontSize: "20pt",
-    padding: "0px 10px"
+  area = {
+    width:"500px",
+    height:"500px",
+    border:"1px solid blue",
   }
 
   constructor(props){
-    super();
+    super(props);
     this.state = {
-      counter: 0,
-      msg: 'Count click:',
-      flg:true
+      list:this.data
     };
     this.doAction = this.doAction.bind(this);
   }
 
   doAction(e){
-    this.setState((state)=>({
-      counter: state.counter + 1,
-      msg: state.counter,
-      flg: !state.flg
-    }));
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({x:x, y:y});
+    this.setState({list:this.data});
+  }
+
+  draw(d) {
+    let s = {
+      position:"absolute",
+      left:(d.x - 25)+"px",
+      top:(d.y - 25)+"px",
+      width:"50px",
+      height:"50px",
+      backgroundColor:"#66f3",
+    };
+    return <div style={s}></div>
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      {this.state.flg ?
-      <p style={this.msgStyle1}>count: {this.state.msg}</p>
-      :
-      <p style={this.msgStyle2}>{this.state.msg}です。</p>
-    }
-      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.data.map((value)=>this.draw(value))}
+      </div>
     </div>;
   }
 }
