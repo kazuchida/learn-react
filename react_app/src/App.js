dc5995ed5a870b5ebc9c7e22355d15ef85b2955e
Kazuhide Uchida <uchidakaz@gmail.com>
2020-05-13 12:53:37 +0900

    chap.3

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,78 +4,58 @@ import './App.css';
 import Rect from './Rect';
 
 
+let theme = {
+  light:{
+    backgroundColor:"#eef",
+    color:"#006",
+    padding:"10px",
+  },
+  dark:{
+    backgroundColor:"#006",
+    color:"#eef",
+    padding:"10px",    
+  }
+};
+
+const ThemeContext = React.createContext(theme.light);
+
 class App extends Component {
+  static contextType = ThemeContext;
 
-  input = '';
-
-  msgStyle = {
-    fontSize:"20pt",
-    color:"#900",
-    margin:"20px 0px",
-    padding:"5px",
+  render(){
+    return (
+      <div style={this.context}>
+        <h1>Context</h1>
+        <Title value="Content page" />
+        <Message value="This is content sample." />
+      </div>
+    );
   }
+}
 
-  inputStyle = {
-    fontSize:"12pt",
-    padding:"5px",
-  }
+class Title extends Component{
+  static contextType = ThemeContext;
 
-  constructor(props){
-    super(props);
-    this.state = {
-      message:'type your name:'
-    };
-    this.doChange = this.doChange.bind(this);
-    this.doSubmit = this.doSubmit.bind(this);
-  }
-
-  doCheck(event){
-    alert(event.target.value + "too logn! (max=10)");
-  }
-
-  doChange(event) {
-    this.input = event.target.value;
-  }
-
-  doSubmit(event) {
-    this.setState({
-      message: 'Hello, '+this.input + '!!'
-    });
-    event.preventDefault();
-  }
-
-  render() {
-    return <div>
-      <h1>React</h1>
-      <h2>{this.state.message}</h2>
-      <Message maxlength="10" onCheck={this.doCheck} />
-    </div>;
+  render(){
+    return(
+      <div>
+        <h2 style={this.context}>{this.props.value}</h2>
+      </div>
+    );
   }
 }
 
 class Message extends Component {
-  li = {
-    fontSize:"16pt",
-    color:"#06",
-    margin:"0px",
-    padding:"0px",
-  }
-
-  constructor(props){
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e){
-    if(e.target.value.length > this.props.maxlength){
-      this.props.onCheck(e);
-      e.target.value = e.target.value.substr(0,this.props.maxlength);
-    }
-  }
+  static contextType = ThemeContext;
 
   render(){
-    return <input type="text" style={this.inputStyle} onChange={this.doChange} />
+    return(
+      <div>
+        <p style={this.context}>{this.props.value}</p>
+      </div>
+    );
   }
 }
+
 
 export default App;
